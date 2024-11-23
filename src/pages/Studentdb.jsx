b53cd0f816ee431811223060c147d6fb6f2f9344
eyeos69 <jjaannnnoooooo@gmail.com>
2024-11-23 14:44:26 +0800

    logout confirmation

--- src/pages/Studentdb.jsx
+++ src/pages/Studentdb.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
-import { Link, Outlet, useNavigate } from "react-router-dom"; // Make sure useNavigate is imported here
+import React, { useState } from "react";
+import LogoutConfirmationModal from "./LogoutConfirmationModal";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import {
   FaBars,
   FaTimes,
